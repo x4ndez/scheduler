82580e7cea4d04ef0f109a6f12f8683daa80709b
x.ande <ananfro@live.com>
2023-06-24 17:28:16 +1000

    Updated move to viewport on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,76 +4,86 @@ const s_hourPeriod = $("#hours");
 //Date
 //Format: Monday, December 13th
 let currentDay = dayjs();
-s_currentDay.text(currentDay.format("dddd, MMMM DD") + "th");
 
 //Present Time
 let presentTime = dayjs();
 let presentTimePeriod = dayjs().format("hA");
 let presentTime24Hr = Number(dayjs().format("HH"));
 
-//for each hour from 9-5...
-//-add past class IF it's less than the current time
-//-add present class IF it's equal to the current time
-//-add future class IF it's more than the current time
+$(function () {
 
-for (let i = 0; i < 9; i++) {
+  //Date
+  s_currentDay.text(currentDay.format("dddd, MMMM DD") + "th");
 
-  let hourAsNumberInLoop = Number(s_hourPeriod.children().eq(i).attr("data-hour")); //Convert string to number
+  //Move viewport to current hour
+  //-IF the current hour is outside the hours of 9-5, then move viewport to the header
+  //-IF the current hour is between 9-5, then move viewport to the current hour
+  if (presentTime24Hr < 9 || presentTime24Hr > 17) {
 
-  if (hourAsNumberInLoop < presentTime24Hr) {
+    console.log(window.location = "#header");
 
-    s_hourPeriod.children().eq(i).addClass("past");
+  }
+  else if (presentTime24Hr >= 9 && presentTime24Hr <= 17) {
 
-  } else if (hourAsNumberInLoop === presentTime24Hr) {
-
-    s_hourPeriod.children().eq(i).addClass("present");
-
-  } else if (hourAsNumberInLoop > presentTime24Hr) {
-
-    s_hourPeriod.children().eq(i).addClass("future");
+    console.log(window.location = "#hour-" + presentTime24Hr.toString());
 
   }
 
-}
+  //for each hour from 9-5...
+  //-add past class IF it's less than the current time
+  //-add present class IF it's equal to the current time
+  //-add future class IF it's more than the current time
 
-//Move to current hour
-window.location = "#hour-" + presentTime24Hr;
+  for (let i = 0; i < 9; i++) {
 
-//Page up event listener
-$("#page-up").on("click", function () {
+    let hourAsNumberInLoop = Number(s_hourPeriod.children().eq(i).attr("data-hour")); //Convert string to number
 
-  window.location = "#header";
+    if (hourAsNumberInLoop < presentTime24Hr) {
 
-});
+      s_hourPeriod.children().eq(i).addClass("past");
 
-//Pull and render from localStorage
+    } else if (hourAsNumberInLoop === presentTime24Hr) {
 
-for (let i = 0; i < 9; i++) {
+      s_hourPeriod.children().eq(i).addClass("present");
 
-  let storageVal = localStorage.getItem(i);
-  s_hourPeriod.children().eq(i).children().eq(1).val(storageVal);
+    } else if (hourAsNumberInLoop > presentTime24Hr) {
 
-}
+      s_hourPeriod.children().eq(i).addClass("future");
 
-//localStorage set on save
+    }
 
-for (let i = 0; i < 9; i++) {
+  }
 
-  s_hourPeriod.children().eq(i).children().eq(2).on("click", function () {
+  //Page up event listener
+  $("#page-up").on("click", function () {
 
-    let inputVal = s_hourPeriod.children().eq(i).children().eq(1).val();
-
-    localStorage.setItem(i, inputVal);
+    window.location = "#header";
 
   });
 
+  //Pull and render from localStorage
 
-}
+  for (let i = 0; i < 9; i++) {
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+    let storageVal = localStorage.getItem(i);
+    s_hourPeriod.children().eq(i).children().eq(1).val(storageVal);
+
+  }
+
+  //localStorage set on save
+
+  for (let i = 0; i < 9; i++) {
+
+    s_hourPeriod.children().eq(i).children().eq(2).on("click", function () {
+
+      let inputVal = s_hourPeriod.children().eq(i).children().eq(1).val();
+
+      localStorage.setItem(i, inputVal);
+
+    });
+
+
+  }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
